3ebfdb78eed088519d6660f806b31903de51f72d
Priyadharshini Bharathan <dharshinee.b963@gmail.com>
2026-05-07 11:27:39 +0000

    SAVE

--- models/stg_employee.sql
+++ models/stg_employee.sql
@@ -1,9 +1,4 @@
-select
-  employee_id,
-  first_name,
-  last_name,
-  current_timestamp as load_time
-from 
-  DBT_1.sch_dbt.src_employees
 
-  select * from DBT_1.SCH_DBT.SRC_EMPLOYEES
+
+  select * from  DBT_1.sch_dbt.src_employees
+
